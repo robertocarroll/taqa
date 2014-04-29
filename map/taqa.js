793c5525264ee1721b2377195f99d9334b7704a6
robertocarroll <robertocarroll@gmail.com>
2014-04-29 17:22:04 +0100

    First version of map

---
+++ map/taqa.js
@@ -0,0 +1,25 @@
+// Load the map 
+var map = L.mapbox.map('map', 'robertocarroll.taqa-2', {
+
+    center: [25, -15],
+    zoom: 2,
+    minZoom: 2,
+    maxZoom: 5,
+    maxBounds: [[-85, -180.0],[85, 180.0]],
+
+    gridControl: false
+});
+
+var activeCountries = L.mapbox.tileLayer('robertocarroll.taqa');
+activeCountries.addTo(map);
+
+var grid = L.mapbox.gridLayer('robertocarroll.taqa');
+
+grid.addTo(map);
+
+grid
+    .on('mousemove',function(o) {
+        document.getElementById('info').innerHTML = (o.data && o.data.scalerank) || '';
+    }).on('mouseout', function(o) {
+        document.getElementById('info').innerHTML = '';
+    });
